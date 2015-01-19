288adf4484d4b643a42148aedf54e61560975799
danielszabo <daniel.szabo@pragbits.com>
2015-01-19 16:10:22 +0100

    images

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using SportsStore.Domain.Abstract;
 using System.Linq;
 using SportsStore.Domain.Entities;
+using System.Web;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -24,12 +25,23 @@ namespace SportsStore.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product) {
-            if (ModelState.IsValid) {
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
                 repository.SaveProduct(product);
                 TempData["message"] = string.Format("{0} has been saved", product.Name);
                 return RedirectToAction("Index");
-            } else {
+            }
+            else
+            {
+                // there is something wrong with the data values
                 return View(product);
             }
         }
